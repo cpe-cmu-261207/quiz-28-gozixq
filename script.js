0b42b993a152bd3fe14d0e3c83648496874d3987
gozixq <natsinee2545@gmail.com>
2022-08-04 14:10:58 +0700

    oooo

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const img = document.querySelector("img-profile")
+//const data = document.querySelector("")
+const btnRandom = document.querySelector("btn-random") 
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+
+}
+
+btnRandom.onclick = async () => { 
+
+  btnRandom.innerText = "Loading..." 
+  btnRandom.disabled = true 
+
+  const resp = await axios.get("https://randomuser.me/api/") 
+  
+  img.src = resp.data.message
+
 }
